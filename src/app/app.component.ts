a30c870c21d07f6bfdf3fbbc4b6a65e1a50f5c00
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-11-17 16:54:24 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+interface AppState {
+  post: Post;
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'voting-system';
+
+  post: Observable<Post>;
+  text!: string;
+  message$: Observable<string>;
+
+  constructor(private store: Store<AppState>) {
+    this.post = this.store.select('post');
+    this.message$ = this.store.select('message');
+  }
+
+  germanMessage() {
+    this.store.dispatch({ type: 'GERMAN' });
+  }
+
+  spanishMessage() {
+    this.store.dispatch({ type: 'SPANISH' });
+  }
+
+  frenchMessage() {
+    this.store.dispatch({ type: 'FRENCH' });
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { simpleReducer } from './reducers/simple.reducer';
+import { FormsModule } from '@angular/forms';
+
+import { postReducer } from './reducers/post.reducer';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({
+      post: postReducer,
+      message: simpleReducer,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, //  Retains last 25 states
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
